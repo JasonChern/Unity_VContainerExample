a9cb01778d3815790e225bd088ad1f76797d6275
jason.chen <jason.chen@xrspace.io>
2023-08-04 11:48:28 +0800

    feat: The base of scope

---
+++ Assets/Scripts/RootScope.cs
@@ -0,0 +1,12 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace unity.vcontainer.example
+{
+    public class RootScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
